c0490738e140f052e88953bdb2b9b8952fac372f
David Martin <davidmartinpassareira@gmail.com>
2023-06-23 11:12:10 +0200

    feat(crear menú): restricciones de semana de cole, comidas de sábado y domingo con nuevo planteamiento de completar solo celdas vacías

--- menu.js
+++ menu.js
@@ -2,128 +2,111 @@
   
   // MATRIZ DE MENÚS
 
-  const comidasNiños = [];
-  const comidasDaia = [];
-  const cenasNiños = [];
-  const cenasDaia = [];
-  const nuevoMenu = [comidasNiños, comidasDaia, cenasNiños, cenasDaia];
+  const comidasPrimerosNiños = Array(7);
+  const comidasSegundosNiños = Array(7);
+  const comidasPrimerosDaia = Array(7);
+  const comidasSegundosDaia = Array(7);
+  const cenasPrimerosNiños = Array(7);
+  const cenasSegundosNiños = Array(7);
+  const cenasPrimerosDaia = Array(7);
+  const cenasSegundosDaia = Array(7);
+  const nuevoMenu = [comidasPrimerosNiños, comidasSegundosNiños, comidasPrimerosDaia, comidasSegundosDaia, cenasPrimerosNiños, cenasSegundosNiños, cenasPrimerosDaia, cenasSegundosDaia];
 
   // FILAS DE LA TABLA DE MENÚS
   
-  const filaComidasNiños = document.querySelectorAll('.comida-niños.añadir-valores');
-  const filaComidasDaia = document.querySelectorAll('.comida-daia.añadir-valores');
-  const filaCenasNiños = document.querySelectorAll('.cena-niños.añadir-valores');
-  const filaCenasDaia = document.querySelectorAll('.cena-daia.añadir-valores');
+  const filaComidasPrimerosNiños = document.querySelectorAll('.comida-primero-niños.añadir-valores');
+  const filaComidasSegundosNiños = document.querySelectorAll('.comida-segundo-niños.añadir-valores');
+  const filaComidasPrimerosDaia = document.querySelectorAll('.comida-primero-daia.añadir-valores');
+  const filaComidasSegundosDaia = document.querySelectorAll('.comida-segundo-daia.añadir-valores');
+  const filaCenasPrimerosNiños = document.querySelectorAll('.cena-primero-niños.añadir-valores');
+  const filaCenasSegundosNiños = document.querySelectorAll('.cena-segundo-niños.añadir-valores');
+  const filaCenasPrimerosDaia = document.querySelectorAll('.cena-primero-daia.añadir-valores');
+  const filaCenasSegundosDaia = document.querySelectorAll('.cena-segundo-daia.añadir-valores');
 
-  // RESTRICCIONES: Asignar a opciones en HOME
-
-    // RESTRICCIÓN 1: SEMANA DE COLE
-
-    let semanaCole = true;
-    let sinComida;
-
-    //document.querySelector('#hora-opt1').classList.contains('seleccionado') ? semanaCole = true : semanaCole = false;
-    sinComida = semanaCole ? 5 : 0;
-
-    // RESTRICCIÓN 2: SÁBADO O DOMINGO COMIDA
-
-    let sabadoComida = true;
-    let domingoComida = true;
-
-    // COMIDAS PARA TODOS
+  // FUNCIÓN PARA CREAR MENÚ
   
   function crearMenu () {                                                                     // La función que crea un nuevo menú
 
     const registros = datosBBDD.registros;                                                    // Hace referencia al objeto registros dentro del objeto BBDD
 
-    //COMIDAS NIÑOS
+    // RESTRICCIÓN 1: SEMANA DE COLE, solo cenas
+
+    let semanaCole = true;
 
     if(semanaCole === true) {
-
-      for(let i = 0; i < 7; i++) {
-
-        if(i < 5) {
-          comidasNiños.push('Sin comida');
-        } else {
-          const objetoAleatorio = registros[Math.floor(Math.random() * registros.length)];        // Selecciona un objeto de forma aleatoria dentro del arreglo de registros
-          const nombreAleatorio = objetoAleatorio[Object.keys(objetoAleatorio)[0]].nombre;        // Obtener el valor de la clave "nombre" del objeto seleccionado
-          comidasNiños.push(nombreAleatorio);                                                     // Incluye el valor del nombre aleatorio en la matriz de comidasNiños
-        }
-        
-        filaComidasNiños[i].innerHTML = comidasNiños[i];
-
-      }} else {
-
-        for(let i = 0; i < 7; i++) {
-
-        const objetoAleatorio = registros[Math.floor(Math.random() * registros.length)];        // Selecciona un objeto de forma aleatoria dentro del arreglo de registros
-        const nombreAleatorio = objetoAleatorio[Object.keys(objetoAleatorio)[0]].nombre;        // Obtener el valor de la clave "nombre" del objeto seleccionado
-        comidasNiños.push(nombreAleatorio);                                                     // Incluye el valor del nombre aleatorio en la matriz de comidasNiños
-        filaComidasNiños[i].innerHTML = comidasNiños[i];                                        // Inserta el valor en la tabla
-        }
+      for(let i = 0; i < 5; i++) {
+        comidasPrimerosNiños[i] = 'Semana de cole';
+        comidasSegundosNiños[i] = 'Semana de cole';
+        comidasPrimerosDaia[i] = 'Semana de cole';
+        comidasSegundosDaia[i] = 'Semana de cole';
       }
-
-    if(sabadoComida) {
-      filaComidasNiños[5].innerHTML = 'Hay comida'; 
     }
 
-    if(domingoComida) {
-      filaComidasNiños[6].innerHTML = 'Hay comida'; 
+    // RESTRICCIÓN 2: SÁBADO O DOMINGO COMIDA
+
+    let sabadoComida = true;
+
+    if(sabadoComida === true) {
+      comidasPrimerosNiños[5] = 'Se come fuera';
+      comidasSegundosNiños[5] = 'Se come fuera';
+      comidasPrimerosDaia[5] = 'Se come fuera';
+      comidasSegundosDaia[5] = 'Se come fuera';
     }
+
+    let domingoComida = true;
+
+    if(domingoComida === true) {
+      comidasPrimerosNiños[6] = 'Se come fuera';
+      comidasSegundosNiños[6] = 'Se come fuera';
+      comidasPrimerosDaia[6] = 'Se come fuera';
+      comidasSegundosDaia[6] = 'Se come fuera';
+    }
+
+    //COMIDAS PRIMEROS NIÑOS
+
   
     //COMIDAS AMAIA & DAVID
-    for(let i = 0; i < 7; i++) {                                                            
-      
-      if(semanaCole === true) {
-
-        for(let i = 0; i < 7; i++) {
-  
-          if(i < 5) {
-            comidasDaia.push('Sin comida');
-          } else {
-            const objetoAleatorio = registros[Math.floor(Math.random() * registros.length)];        // Selecciona un objeto de forma aleatoria dentro del arreglo de registros
-            const nombreAleatorio = objetoAleatorio[Object.keys(objetoAleatorio)[0]].nombre;        // Obtener el valor de la clave "nombre" del objeto seleccionado
-            comidasDaia.push(nombreAleatorio);                                                     // Incluye el valor del nombre aleatorio en la matriz de comidasNiños
-          }
-          
-          filaComidasDaia[i].innerHTML = comidasDaia[i]; 
-  
-        }} else {
-  
-          for(let i = 0; i < 7; i++) {
-          const objetoAleatorio = registros[Math.floor(Math.random() * registros.length)];        // Selecciona un objeto de forma aleatoria dentro del arreglo de registros
-          const nombreAleatorio = objetoAleatorio[Object.keys(objetoAleatorio)[0]].nombre;        // Obtener el valor de la clave "nombre" del objeto seleccionado
-          comidasDaia.push(nombreAleatorio);                                                     // Incluye el valor del nombre aleatorio en la matriz de comidasNiños
-          filaComidasDaia[i].innerHTML = comidasDaia[i];                                        // Inserta el valor en la tabla
-          }
-        }                                        
-      }
-
-    if(sabadoComida) {
-      filaComidasDaia[5].innerHTML = 'Hay comida'; 
-    }
-
-    if(domingoComida) {
-      filaComidasDaia[6].innerHTML = 'Hay comida'; 
-    }
+    
 
     //CENAS NIÑOS
-    for(let i = 0; i < 7; i++) {
-      
-        const objetoAleatorio = registros[Math.floor(Math.random() * registros.length)];
-        const nombreAleatorio = objetoAleatorio[Object.keys(objetoAleatorio)[0]].nombre;
-        cenasNiños.push(nombreAleatorio);
-        filaCenasNiños[i].innerHTML = cenasNiños[i];
-      }
+    
 
     //CENAS AMAIA & DAVID
-    for(let i = 0; i < 7; i++) {                                                         
-      
-        const objetoAleatorio = registros[Math.floor(Math.random() * registros.length)];     
-        const nombreAleatorio = objetoAleatorio[Object.keys(objetoAleatorio)[0]].nombre;    
-        cenasDaia.push(nombreAleatorio);                                                  
-        filaCenasDaia[i].innerHTML = cenasDaia[i];
-      }
+   
+
+    // VOLVAR MATRIZ EN TABLA
+    console.log(nuevoMenu);
+    for(let z = 0; z < comidasPrimerosNiños.length; z++) {                                    // Vuelca la fila de comidasPrimerosNiños
+      filaComidasPrimerosNiños[z].innerHTML = comidasPrimerosNiños[z];
+    }
+
+    for(let z = 0; z < comidasSegundosNiños.length; z++) {                                    // Vuelca la fila de comidasSegundosNiños
+      filaComidasSegundosNiños[z].innerHTML = comidasSegundosNiños[z];
+    }
+
+    for(let z = 0; z < comidasPrimerosDaia.length; z++) {                                    // Vuelca la fila de comidasPrimerosDaia
+      filaComidasPrimerosDaia[z].innerHTML = comidasPrimerosDaia[z];
+    }
+
+    for(let z = 0; z < comidasSegundosDaia.length; z++) {                                    // Vuelca la fila de comidasSegundosDaia
+      filaComidasSegundosDaia[z].innerHTML = comidasSegundosDaia[z];
+    }
+
+    for(let z = 0; z < cenasPrimerosNiños.length; z++) {                                    // Vuelca la fila de cenasPrimerosNiños
+      filaCenasPrimerosNiños[z].innerHTML = cenasPrimerosNiños[z];
+    }
+
+    for(let z = 0; z < cenasSegundosNiños.length; z++) {                                    // Vuelca la fila de cenasSegundosNiños
+      filaCenasSegundosNiños[z].innerHTML = cenasSegundosNiños[z];
+    }
+
+    for(let z = 0; z < cenasPrimerosDaia.length; z++) {                                    // Vuelca la fila de cenasPrimerosDaia
+      filaCenasPrimerosDaia[z].innerHTML = cenasPrimerosDaia[z];
+    }
+
+    for(let z = 0; z < cenasSegundosDaia.length; z++) {                                    // Vuelca la fila de cenasSegundosDaia
+      filaCenasSegundosDaia[z].innerHTML = cenasSegundosDaia[z];
+    }
   
   }
   
